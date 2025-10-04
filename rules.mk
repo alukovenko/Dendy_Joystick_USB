e770eb7a256ced36408872cb97abf68bd0d6dd00
Andrei Lukovenko <andrei.lukovenko@gmail.com>
2025-10-04 21:06:32 +0100

    blinker

--- rules.mk
+++ rules.mk
@@ -110,7 +110,7 @@ LDLIBS += -Wl,--start-group -lc -lgcc -lnosys -Wl,--end-group
 %: s.%
 %: SCCS/s.%
 
-all: $(PROJECT).elf $(PROJECT).bin
+all: $(BUILD_DIR)/$(PROJECT).elf $(BUILD_DIR)$(PROJECT).bin
 flash: $(PROJECT).flash
 
 # error if not using linker script generator
